1c5ef7d5174d2baa69b1889f698ef4f95ca2bc05
Alim Mamutov <mamutov.creation@yandex.ru>
2021-03-17 14:55:14 +0300

    Initialize new lesson

---
+++ main.py
@@ -0,0 +1,12 @@
+# функция enumerate
+
+# winners = ['Leo', 'Max', 'Kate']
+# en = enumerate(winners, 1)
+# for number, winner in enumerate(winners, 1):
+#     print(number, winner)
+
+def print_sep():
+    print('=' * 100)
+
+print_sep()
+#28/26
